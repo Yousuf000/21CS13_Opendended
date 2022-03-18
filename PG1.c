3cfc167165fe6419886dd5f20fe149a033ac3df3
Yousuf000 <101882305+Yousuf000@users.noreply.github.com>
2022-03-18 20:20:22 +0530

    Update PG1.c

--- PG1.c
+++ PG1.c
@@ -1,3 +1,23 @@
-//Program to check vowel or not 
-//Input :a
-//Output:a is a vowel
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+    int lowercase_Vowel, uppercase_Vowel;
+
+    printf("Please Enter an alphabet: \n");
+    scanf(" %c", &ch);
+
+    lowercase_Vowel = (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+    uppercase_Vowel = (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U');
+
+    if (lowercase_Vowel || uppercase_Vowel) {
+    	printf("\n %c is a VOWEL.", ch);
+	}
+    else {
+    	printf("\n %c is a CONSONANT.", ch);
+	}
+    return 0;
+}
+
+
